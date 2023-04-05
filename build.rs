12eb5d7ce8b23f14829f5db974d8c37b0c2d22f0
Vibhav Pant <vibhavp@gmail.com>
2023-04-05 19:31:55 +0530

    Add additional atoms.

--- build.rs
+++ build.rs
@@ -5,10 +5,13 @@ use std::path::Path;
 
 fn main() {
     string_cache_codegen::AtomType::new("parser::atoms::KnownIdentifierAtom", "ident_atom!")
+        .atoms(&["+", "-", "*", "/", "<", "=", ">"])
+        .atoms(&["...", "!", "$", "%", "&", ":", "?", "~", "_", "^"])
         .atoms(&[
-            "+", "-", "...", "!", "$", "%", "&", "*", "/", ":", "<", "=", ">", "?", "~", "_", "^",
+            "pair?", "list?", "cons", "car", "cdr", "set-car!", "set-cdr!", "null?",
         ])
-        .atoms(&["cons", "car", "cdr", "set-car!", "set-cdr!", "null?"])
+        .atoms(&["number?", "complex?", "real?", "rational?", "integer?"])
+        .atoms(&["boolean?"])
         .write_to_file(&Path::new(&env::var("OUT_DIR").unwrap()).join("identifier_atom.rs"))
         .unwrap()
 }
